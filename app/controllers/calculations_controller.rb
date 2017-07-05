3c9668e2df46779d3f08f2fafcbdc3efeffbb764
Zuka <zmargvel@chicagobooth.edu>
2017-07-05 23:17:28 +0000

    first test commit

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -11,7 +11,7 @@ class CalculationsController < ApplicationController
     # ================================================================================
 
 
-    @word_count = "Replace this string with your answer."
+    @word_count = "Replace this string with your answer.".length.to_s
 
     @character_count_with_spaces = "Replace this string with your answer."
 
