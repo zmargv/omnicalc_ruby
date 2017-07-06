7a17282a7fde48aeb5e59ac38d37ff09b398d701
Zuka <zmargvel@chicagobooth.edu>
2017-07-06 00:58:02 +0000

    attempted solution for time between

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -38,7 +38,11 @@ class CalculationsController < ApplicationController
     # The principal value the user input is in the decimal @principal.
     # ================================================================================
 
-    @monthly_payment = "Replace this string with your answer."
+    @monthly_payment = if @apr==0 
+      @principal/@years/12
+    else 
+      @apr/1200*@principal / (1-(1+@apr/1200)**(-@years*12))
+    end
 
     # ================================================================================
     # Your code goes above.
@@ -60,12 +64,12 @@ class CalculationsController < ApplicationController
     #   number of seconds as a result.
     # ================================================================================
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = @ending - @starting
+    @minutes = @seconds/60
+    @hours = @minutes/60
+    @days = @hours/24
+    @weeks = @days/7
+    @years = @days/365.25
 
     # ================================================================================
     # Your code goes above.
